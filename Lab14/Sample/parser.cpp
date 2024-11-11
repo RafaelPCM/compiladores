2dedbe436bd240b51c30e3f001f4a8ec5e9335c7
RafaelPCM <rafaelpcmaia1@hotmail.com>
2024-11-11 03:03:51 -0300

    modificacoes feitas

--- Lab14/Sample/parser.cpp
+++ Lab14/Sample/parser.cpp
@@ -14,6 +14,7 @@ void Parser::Start()
             case IF: cout << "IF\n"; break;
             case THEN: cout << "THEN\n"; break;
             case ELSE: cout << "ELSE\n"; break;
+            case WHILE: cout << "WHILE\n"; break;
             case ID: cout << "ID: " << scanner.YYText() << "\n"; break;
             case NUM: cout << "NUM: " << scanner.YYText() << "\n"; break;
             case RELOP: cout << "RELOP: " << scanner.YYText() << "\n";; break;
